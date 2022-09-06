1cb85083afd1c942f012eae2f2e01d66322070ba
duckduckquack <elogins34@tutanota.com>
2022-09-06 10:01:58 +0200

    Add documentation for .R files.

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,5 +1,29 @@
+#' Dijkstra algorithm.
+#' \code{dijkstra} returns the shortest path between \code{init_node}
+#' and all the other nodes in \code{graph}.
+#'
+#' @param graph A data frame representing an undirected weighted graph.
+#'  The first column fo the data frame v1 lists all the nodes repeated
+#'  as many times as they have neighbors. The second column of the data
+#'  frame v2 lists per each entry in v1 its neighbor. And the last column
+#'  w lists the edge between corresponding entries in v1 and v2. See
+#'  \code{wiki_graph} in example.
+#' @param init_node An integer representing a node in \code{graph}.
+#'
+#' @return Returns a vector of shortest path distances between
+#'  \code{init_node} and all the other nodes in \code{graph}.Each
+#'  entry represents the distance between \code{init_node} and the
+#'  node that the entry represents.
+#' @export
+#' @references https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+#' @examples
+#' wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#' v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#' w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#' dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 3)
 dijkstra <- function(graph, init_node) {
-  
+
   #vector of nodes
   nodes <- unique(graph$v1)
   #vector of distances from init_node
@@ -7,10 +31,10 @@ dijkstra <- function(graph, init_node) {
   df <- data.frame(nodes = nodes, d = d)
   #vector of unvisited nodes
   u <- nodes
-  
+
   current <- init_node
   while(TRUE) {
-    #get vertex with smallest known distance from 
+    #get vertex with smallest known distance from
     #init_node
     #neighbors of current node
     neigh <- graph[graph$v1 == current, 2]
@@ -28,7 +52,7 @@ dijkstra <- function(graph, init_node) {
     if(length(u) == 0){
       break
     }
-    m <- min(df$d[u]) 
+    m <- min(df$d[u])
     current <- df[u, ][df[u, ]$d == m, 1][1]
   }
   df$d
--- R/euclidean.R
+++ R/euclidean.R
@@ -1,19 +1,35 @@
+#' Euclidean algorithm.
+#' \code{euclidean} returns the GCD of the two arguments.
+#' This function uses the Euclidean algorithm to compute
+#' the GCD of the two arguments.
+#' @param x,y Integer numbers.
+#'
+#'
+#' @return Returns the GCD of x and y as an integer number.
+#' @export
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
+#' @examples
+#' euclidean(45L, 10L)
+#' euclidean(10L, 5L)
+#' \dontrun{
+#' euclidean("b", "a")
+#' }
 euclidean <- function(x, y) {
   #finds GCD of x and y, x and y must be integers
-  
-  #check that x and y are integers 
+
+  #check that x and y are integers
   if(!(is.integer(x) & is.integer(y))) {
     stop("Inputs must be integers")
   }
-  
+
   a <- max(x, y)
   b <- min(x, y)
-  
+
   #base case
   if(b == 0){
     return(a)
   }
-  
+
   #recursive case
   return(euclidean(b, a %% b))
 }
